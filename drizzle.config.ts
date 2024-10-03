e705d2dc33e095d35a196937c3c4e968409586f6
BrunaSepulveda <sepulvedabruna@gmail.com>
2024-10-03 17:35:06 -0300

    retorna a ultima forma que a api esta funcionando

--- drizzle.config.ts
+++ drizzle.config.ts
@@ -1,11 +1,11 @@
-import { defineConfig } from "drizzle-kit";
-import { env } from "./src/env.ts";
+import { defineConfig } from 'drizzle-kit'
+import { env } from './src/env.ts'
 
 export default defineConfig({
-  schema: "./src/db/schema.ts",
-  out: "./migrations",
-  dialect: "postgresql",
+  schema: './src/db/schema.ts',
+  out: './migrations',
+  dialect: 'postgresql',
   dbCredentials: {
     url: `${env.DB_TYPE}://${env.DB_USER}:${env.DB_PASSWORD}@${env.DB_HOST}:${env.DB_PORT}/${env.DB_NAME}`,
   },
-});
+})
