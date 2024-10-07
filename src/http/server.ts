33410f3797d0f100730fb5a8203dfdf47d3809a7
BrunaSepulveda <sepulvedabruna@gmail.com>
2024-10-07 17:27:42 -0300

    altera configurações da porta

--- src/http/server.ts
+++ src/http/server.ts
@@ -23,7 +23,9 @@ app.register(fastifyCors, {
   origin: '*',
 })
 
-app.listen({ port: 3333 }, (err, address) => {
+const PORT = Number(process.env.PORT) || 3333
+
+app.listen({ port: PORT, host: '0.0.0.0' }, (err, address) => {
   if (err) {
     app.log.error(err)
     process.exit(1)
