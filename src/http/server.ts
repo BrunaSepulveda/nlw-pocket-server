08d9c1e4a36ba042b39f7e74bc799633b5e71255
BrunaSepulveda <sepulvedabruna@gmail.com>
2024-10-07 17:38:14 -0300

    fix

--- src/http/server.ts
+++ src/http/server.ts
@@ -23,9 +23,7 @@ app.register(fastifyCors, {
   origin: '*',
 })
 
-const PORT = Number(process.env.DB_PORT) || 3333
-
-app.listen({ port: PORT, host: process.env.DB_HOST }, (err, address) => {
+app.listen({ port: 3333, host: '0.0.0.0' }, (err, address) => {
   if (err) {
     app.log.error(err)
     process.exit(1)
