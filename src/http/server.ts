cb1c987b6a3b358676f24fab41c6b769e57fdf21
BrunaSepulveda <sepulvedabruna@gmail.com>
2024-10-07 17:32:16 -0300

    altera a forma de acessar as portas

--- src/http/server.ts
+++ src/http/server.ts
@@ -23,9 +23,9 @@ app.register(fastifyCors, {
   origin: '*',
 })
 
-const PORT = Number(process.env.PORT) || 3333
+const PORT = Number(process.env.DB_PORT) || 3333
 
-app.listen({ port: PORT, host: '0.0.0.0' }, (err, address) => {
+app.listen({ port: PORT, host: process.env.DB_HOST }, (err, address) => {
   if (err) {
     app.log.error(err)
     process.exit(1)
